081192e6c987c424b464c7e20e7a7c1aa8cab543
Jean-Paul Beaudonnet <jpjp03911@gmail.com>
2021-12-29 19:19:31 +0100

    fonctionnalité de gestion du header depuis le backoffice

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Classe\Mail;
+use App\Entity\Header;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,10 +24,13 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+
         $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
+        $headers = $this->entityManager->getRepository(Header::class)->findAll();
 
         return $this->render('home/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'headers' => $headers
         ]);
     }
 }
